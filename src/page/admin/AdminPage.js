46ecabbebd24e60a54fcc4c040682592607d004a
BangMinah-Dev <huytq.act@gmail.com>
2021-05-13 18:15:17 +0700

    upload

--- src/page/admin/AdminPage.js
+++ src/page/admin/AdminPage.js
@@ -1,17 +1,32 @@
-import LayoutAdmin from "../../layouts/LayoutAdmin"
-import {useHistory} from "react-router-dom"
+import LayoutAdmin from "../../layouts/LayoutAdmin";
+import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getProducts } from "../../API/api";
 export default function Admin() {
   document.title = "START GAME - ADMIN";
 
-  const history = useHistory()
-  if(localStorage.getItem("token") === null){
-    history.push("/login")
+  const history = useHistory();
+  if (localStorage.getItem("token") === null) {
+    history.push("/login");
   }
 
+  const [totalProduct, setTotalProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await getProducts();
+      setTotalProduct(res);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="container-fluid">
-      <LayoutAdmin/>
-      <h3 className="text-center">QUẢN LÝ CHUNG</h3>
+    <div>
+      <LayoutAdmin />
+      <div className="show-contents">
+        <h3 className="mt-2 mb-4">QUẢN LÝ CHUNG</h3>
+        <div>Số lượng game : {totalProduct.length}</div>
+      </div>
     </div>
   );
 }
