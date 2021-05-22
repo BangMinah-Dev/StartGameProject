6dcc55cde38ab65c9dada878e204d000ed2fa1ae
BangMinah-Dev <huytq.act@gmail.com>
2021-05-22 12:02:00 +0700

    upload

--- src/page/admin/profile/AdminProfile.js
+++ src/page/admin/profile/AdminProfile.js
@@ -4,7 +4,6 @@ import LayoutAdmin from "../../../layouts/LayoutAdmin";
 import { getAdminProfile, UPLOAD_PATH } from "../../../API/api";
 import { Button } from "react-bootstrap";
 export default function AdminProfile() {
-
   const [adminProfile, setAdminProfile] = useState([{}]);
 
   const adminID = Number(localStorage.getItem("adminID"));
@@ -36,12 +35,14 @@ export default function AdminProfile() {
         <div className="row d-flex justify-content-center">
           <div className="col-md-2">
             <div className="admin-label">
-              <p>Ảnh đại diện</p>
+              Ảnh đại diện
             </div>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-5">
             <div className="admin-avatar d-flex flex-column">
-              {arr !== "" && <img src={ UPLOAD_PATH + arr[0]?.avatar} alt="Avatar"></img>}
+              {arr !== "" && (
+                <img src={UPLOAD_PATH + arr[0]?.avatar} alt="Avatar"></img>
+              )}
               <Button className="btn mt-2 change-avatar">Thay Ảnh</Button>
             </div>
           </div>
@@ -51,8 +52,10 @@ export default function AdminProfile() {
           <div className="col-md-2">
             <div className="admin-label">Email : </div>
           </div>
-          <div className="col-md-3">
-            <div>{arr[0]?.email}</div>
+          <div className="col-md-5">
+            <div className="admin-content">
+              {arr[0]?.email}
+            </div>
           </div>
         </div>
 
@@ -60,15 +63,19 @@ export default function AdminProfile() {
           <div className="col-md-2">
             <div className="admin-label">Mật khẩu : </div>
           </div>
-          <div className="col-md-3">{arr[0]?.password}</div>
+          <div className="col-md-5">
+            <div className="admin-content">
+              {arr[0]?.password}
+            </div>
+          </div>
         </div>
 
         <div className="row d-flex justify-content-center">
           <div className="col-md-2">
             <div className="admin-label">Số điện thoại : </div>
           </div>
-          <div className="col-md-3">
-            <div>{arr[0]?.phone}</div>
+          <div className="col-md-5">
+            <div className="admin-content">{arr[0]?.phone}</div>
           </div>
         </div>
       </div>
